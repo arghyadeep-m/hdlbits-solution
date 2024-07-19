e7548ef63fb971dbef9c15b6f77c70fc506d38a9
arghyadeep-m <121192597+arghyadeep-m@users.noreply.github.com>
2024-07-19 14:13:58 +0530

    Solution to question 115, ece241_2013_q12

---
+++ 115_ece241_2013_q12/top_module.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input enable,
+    input S,
+    input A, B, C,
+    output Z 
+); 
+    reg [0:7] q;
+    assign Z = q[{A,B,C}];
+    always @(posedge clk) begin
+        if (enable)
+            q = {S, q[0:6]};
+    end
+endmodule
